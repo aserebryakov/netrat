ef98984e67e9d294c8e09bd80f1a64fcd07ed8cd
Alexander Serebryakov <alex.serebr@gmail.com>
2018-06-05 22:11:25 +0300

    Adds the Config structure.

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+use std::num::ParseFloatError;
+
+pub struct Rate {
+    pub value: Option<f64>,
+}
+
+impl std::str::FromStr for Rate {
+    type Err = ParseFloatError;
+
+    fn from_str(s: &str) -> Result<Rate, ParseFloatError> {
+        Ok(Rate{value: f64::from_str(&s).ok()})
+    }
+}
+
+pub struct Config {
+    pub host: String,
+        pub port: String,
+        pub rate: Rate,
+        pub input: String,
+}
+
+impl Config {
+    pub fn new() -> Config {
+        Config {
+host: String::from("127.0.0.1"),
+          port: String::from("40000"),
+          rate: Rate{value: None},
+          input: String::new(),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,55 @@
 extern crate argparse;
+extern crate netrat;
 
-use std::io::{self, Write, Read, ErrorKind};
+use std::io::{self, Write, Read};
 use std::net::TcpStream;
 use std::{thread, time};
 use argparse::{ArgumentParser, Store};
 use std::fs::File;
-use std::num::ParseFloatError;
+use netrat::Config;
 
 
 fn main() {
-    let mut host = String::from("127.0.0.1");
-    let mut port = String::from("40000");
-    let mut rate = Rate{value: None};
-    let mut input = String::new();
+    let mut config = Config::new();
 
     {
         let mut parser = ArgumentParser::new();
 
         parser.set_description("netrat is like netcat but not that good.");
 
-        parser.refer(&mut host).add_argument("host", Store, "Remote host").required();
-        parser.refer(&mut port).add_argument("port", Store, "Remote port").required();
-        parser.refer(&mut rate).add_option(&["-r", "--rate"], Store, "Data transfer rate");
-        parser.refer(&mut input).add_option(&["-i", "--input"], Store, "Input file");
+        parser.refer(&mut config.host).add_argument("host", Store, "Remote host").required();
+        parser.refer(&mut config.port).add_argument("port", Store, "Remote port").required();
+        parser.refer(&mut config.rate).add_option(&["-r", "--rate"], Store, "Data transfer rate");
+        parser.refer(&mut config.input).add_option(&["-i", "--input"], Store, "Input file");
         parser.parse_args_or_exit();
     }
 
-    let target = format!("{}:{}", host, port);
+    let target = format!("{}:{}", config.host, config.port);
     println!("Connecting to {}...", target);
 
     let mut stream = TcpStream::connect(target).unwrap();
 
-    loop {
-        let data = read_data(&input).unwrap();
-        let interval = match rate.value {
-            Some(r) => time::Duration::from_millis((1024f64 / r) as u64),
-            None => time::Duration::from_millis(0),
-        };
+    let interval = match config.rate.value {
+        Some(r) => time::Duration::from_millis((1024f64 / r) as u64),
+        None => time::Duration::from_millis(0),
+    };
 
+    loop {
+        let data = read_data(&config.input).unwrap();
         send_data(&mut stream, data.as_slice(), interval);
+        break;
     }
 }
 
 
 fn read_data(input: &String) -> Result<Vec<u8>, std::io::Error> {
-    let mut out = Vec::<u8>::new();
-
     if input.is_empty() {
-        Err(std::io::Error::new(ErrorKind::Other, "Not Implemented"))
+        let mut line = String::new();
+        io::stdin().read_line(&mut line)?;
+        Ok(line.into_bytes())
     }
     else {
+        let mut out = Vec::<u8>::new();
         let mut file = File::open(input.as_str())?;
         file.read_to_end(&mut out)?;
         Ok(out)
@@ -68,15 +68,3 @@ fn send_data(stream: &mut TcpStream, data: &[u8], interval: time::Duration) {
     }
 }
 
-
-struct Rate {
-    pub value: Option<f64>,
-}
-
-impl std::str::FromStr for Rate {
-    type Err = ParseFloatError;
-
-    fn from_str(s: &str) -> Result<Rate, ParseFloatError> {
-        Ok(Rate{value: f64::from_str(&s).ok()})
-    }
-}
